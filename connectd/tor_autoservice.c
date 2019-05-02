a2e3aa2ba3bae4e9f3af44f696cf305316ac6f10
Jeff Vandrew Jr <jeffvandrew@yahoo.com>
2019-05-02 13:07:00 -0400

    replace string literal

--- connectd/tor_autoservice.c
+++ connectd/tor_autoservice.c
@@ -82,11 +82,13 @@ static struct wireaddr *make_onion(const tal_t *ctx,
 	char *line;
 	const char *private_key_blob_path;
 	const char *private_key_blob_file;
+	const char *blob_file_name;
 	struct wireaddr *onion;
 
 //V3 tor after 3.3.3.aplha FIXME: TODO SAIBATO
 //sprintf((char *)reach->buffer,"ADD_ONION NEW:ED25519-V3 Port=9735,127.0.0.1:9735\r\n");
-	private_key_blob_path = path_join(NULL, lightning_dir, ".keyblob");
+	blob_file_name = ".keyblob"
+	private_key_blob_path = path_join(tmpctx, lightning_dir, blob_file_name);
 	private_key_blob_file = grab_file(tmpctx, private_key_blob_path);
 	if (!private_key_blob_file) {
 		// if no keyblob file exists, have Tor generate a keyblob
